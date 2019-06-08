a1fc85cab6cf19f4ed5501a692f158e285938503
Reiner Gerecke <me@reinergerecke.de>
2019-06-08 06:34:29 +0200

    Send received data to carbon agent via plaintext protocol

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::net::{UdpSocket};
+use std::io::Write;
+use std::net::{TcpStream, UdpSocket};
 use std::str;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug)]
 enum MessageKind {
@@ -41,14 +43,19 @@ fn parse_package(buf: &[u8]) -> Vec<Message> {
 }
 
 fn main() {
+    let mut carbon = TcpStream::connect("127.0.0.1:2003").unwrap();
     let socket = UdpSocket::bind("127.0.0.1:8125").unwrap();
 
     loop {
         let mut buf = [0; 8192];
         let amount = socket.recv(&mut buf).unwrap();
         let metrics = parse_package(&buf[..amount]);
+
+        let start = SystemTime::now();
+        let unix = start.duration_since(UNIX_EPOCH).unwrap();
         for m in metrics {
             println!("metric key={} value={} kind={:?}", m.key, m.value, m.kind);
+            write!(carbon, "{} {} {}\n", m.key, m.value, unix.as_secs()).unwrap();
         }
     }
 }
